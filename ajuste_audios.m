619538a67425a50c6ef6f3177330430b3c70accf
LucasAssis00 <92377740+LucasAssis00@users.noreply.github.com>
2023-06-27 16:56:01 -0300

    Update ajuste_audios.m

--- ajuste_audios.m
+++ ajuste_audios.m
@@ -12,6 +12,6 @@ X1 = x1 + 0,25*x2 + 0.35*x3
 X2 = 0.38*x1 + 1,25*x2 + 0.12*x3
 X3 = 0.4*x1 + 0,22*x2 + 1.35*x3
 
-audiowrite('contagem_sep1.wav', X1, fs1);
-audiowrite('contagem_sep1.wav', X2, fs2);
-audiowrite('contagem_sep1.wav', X3, fs3);
+audiowrite('contagem_mix1.wav', X1, fs1);
+audiowrite('contagem_mix2.wav', X2, fs2);
+audiowrite('contagem_mix3.wav', X3, fs3);
